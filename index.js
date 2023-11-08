b08b42dfb615590e425a5c5fe75a716fafa158fe
LEESEUNGMINI <dltmdals7295@naver.com>
2023-11-08 13:19:25 +0900

    javascript variables

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello js")
+// 변수
+const whoAmI = "이승민"
+let hobby = "달리기"
+
+const a = 10
+const b = 20
+
+
+console.log(typeof whoAmI)
+console.log(typeof a)
+
+// console.log("나의 이름은 " + whoAmI + "이고, 나의 취미는 " + boddy + "입니다")
+// console.log(`나의 이름은 ${whoAmI} 이고, 나의 취미는 ${bobby} 입니다.`)
+
+const arr = [1, 2, 3]
+console. log(arr[1])
+
+const 승민 = "나는"
+const 입니다 = "아닙니다"
+
